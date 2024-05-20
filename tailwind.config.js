96b67abdfbb482d758f12adb9d5eb0a95629d57f
devcowsar <kawsarcse836@gmail.com>
2024-05-20 17:33:23 +0600

    fix: customcolortalwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'primary': '#FFE6AD',
+      'secondary': '#6C63FF',
+      'heading-text':'#383838',
+      'body-text': '#545454',
+      'white-text': '#FFFFFF',
+      'bgcolor-one': '#FED29C',
+      'bgcolor-two': '#FFEAD0',
+      'bgcolor-three': '#FFF9F1',
+      'bgcolor-four': '#FFC3C6',
+      'bgcolor-five': '#EE8388',
+      'bgcolor-six': '#FFF1F2',
+    },
     extend: {},
   },
   plugins: [],
