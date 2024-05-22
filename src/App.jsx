a9fe15020179d26899afc4a6d254f5893929470d
devcowsar <kawsarcse836@gmail.com>
2024-05-22 14:44:15 +0600

    feat: slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import About from "./Components/About";
 import DiscountProduct from "./Components/DiscountProduct";
 import Layout from "./Components/Layout";
+import Slider from "./Components/Slider";
 
 function App() {
   return (
     <Layout>
+      <Slider />
       <DiscountProduct />
       <About />
     </Layout>
